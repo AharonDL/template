f39347d6c56c8f3e7bbbcc48897718a313d3244d
aharon@dataloop.ai <aharon@dataloop.ai>
2023-09-19 15:17:51 +0300

    v1.0.2

--- modules/main.py
+++ modules/main.py
@@ -1,19 +1,5 @@
 import dtlpy as dl
-from modules.compilerFactory import CompilerFactory
-from modules.emailDispatcher import EmailDispatcher
-from modules.notificationInfo import ApplicationInput
-from modules.templateResolver import TemplateResolver
 
-
-class ServiceRunner(dl.BaseServiceRunner):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-    def email(self, input: dict):
-        application_input = ApplicationInput(input)
-        if application_input.recipients is None or len(application_input.recipients) == 0:
-            return
-        template = TemplateResolver.resolve_template(application_input.notification_info.notification_code)
-        compiler = CompilerFactory.get_compiler(template=template, application_input=application_input)
-        [compiled_html, attachments] = compiler.compile_html(template=template)
-        EmailDispatcher.dispatch(application_input=application_input, compiled_html=compiled_html, attachments=attachments)
+class ServiceRunner(dl.BaseModelAdapter):
+    def __init__(self, model):
+        super().__init__(model=model)
